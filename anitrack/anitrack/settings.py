1e0b86e892a3738defb4d813402be4db7fb9ff48
dmitriibeliakov <dmitrii-bel@duzavr.com>
2026-09-06 22:54:05 +0300

    spectacular schema

--- anitrack/anitrack/settings.py
+++ anitrack/anitrack/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_spectacular',
     'main',
     'users',
 ]
@@ -31,6 +32,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
