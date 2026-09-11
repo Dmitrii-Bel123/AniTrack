bbdccc5df8ad5e59d532425626b151ff94aeacd5
dmitriibeliakov <dmitrii-bel@duzavr.com>
2026-09-11 20:10:59 +0300

    rework structure

--- backend/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Anime, Genre, UserAnime
-
-# Register your models here.
-
-admin.site.register(Anime)
-admin.site.register(Genre)
-admin.site.register(UserAnime)
